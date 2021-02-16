2c49dd2c53976f4806b63292ee7dc7c899ea8da3
Batoch <baptiste.massardier@gmail.com>
2021-02-16 16:03:24 +0100

    Block and Chain

--- block.py
+++ block.py
@@ -5,4 +5,60 @@ from key import verify_signature
 
 
 class Block:
-    pass
+
+    def __init__(self, index, previous_hash, nonce=0, timestamp=0, transactions=[], hashval=None, miner=None):
+        self.index = index
+        self.nonce = nonce
+        self.timestamp = timestamp
+        self.transactions = []
+        for elem in transactions:
+            self.transactions.append(Transaction(elem["sender"], elem["receiver"], elem["amount"], elem["timestamp"],
+                                                 elem["tx_number"]))
+        self.previous_hash = previous_hash
+        self.hashval = hashval
+        self.miner = miner
+
+    def hash(self, nonce):
+        sha = hashlib.sha256()
+        data = ""
+        data = str(self.index) + str(self.timestamp) + str(nonce) + str(self.previous_hash)
+        for elem in self.transactions:
+            data += str(elem.sender) + str(elem.receiver) + str(elem.amount)
+        sha.update(data.encode())
+        return sha.hexdigest()
+
+    def add_transaction(self, transaction):
+        tx = transaction
+        tx.tx_number = len(self.transactions)
+        self.transactions.append(tx)
+        pass
+
+    def to_dict(self):
+        block_dict = {}
+        block_dict["index"] = self.index
+        block_dict["nonce"] = self.nonce
+        block_dict["timestamp"] = self.timestamp
+        block_dict["miner"] = self.miner
+        block_dict["transactions"] = []
+        for elem in self.transactions:
+            block_dict["transactions"].append(elem.to_dict())
+        block_dict["previous_hash"] = self.previous_hash
+        block_dict["hashval"] = self.hashval
+        return block_dict
+
+    def mine(self, difficulty):
+        self.timestamp = time.time()
+        nonce = 0
+        prefix = "0" * difficulty
+        hash_value = self.hash(nonce)
+        print("Debut du minage")
+        while (hash_value.startswith(prefix) == False):
+            nonce += 1
+            hash_value = self.hash(nonce)
+            print(hash_value)
+        print("Le minage est terminé")
+        self.hashval = hash_value
+        self.nonce = nonce
+        self.miner = "밥"
+
+        return nonce
--- transaction.py
+++ transaction.py
@@ -2,4 +2,24 @@ from key import verify_signature
 
 
 class Transaction:
+
+    def __init__(self, amount, receiver, sender, timestamp = 0, tx_number = None):
+        self.amount = amount
+        self.receiver = receiver
+        self.sender = sender
+        self.timestamp = timestamp
+        self.tx_number = tx_number
+
+    def __repr__(self):
+        string = "Transaction number: " + str(self.tx_number) + "\n" + "Sender: " + str(self.sender) + "\n" + "Receiver: " + str(self.receiver) + "\n" + "Amount: " + str(self.amount) + "\n" + "Timestamp: " + str(self.timestamp) + "\n"
+        return string
+
+    def to_dict(self):
+        tx_dict = {}
+        tx_dict["tx_number"] = self.tx_number
+        tx_dict["sender"] = self.sender
+        tx_dict["receiver"] = self.receiver
+        tx_dict["amount"] = self.amount
+        tx_dict["timestamp"] = self.timestamp
+        return tx_dict
     pass
